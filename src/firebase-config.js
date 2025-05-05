95ab140f3b8b74c9a077ae3e081f3eee6c5662c0
Guillaume SAINT-ETIENNE <guillaumesaint-etienne@MacBook-Air-de-Guillaume.local>
2025-05-05 12:16:15 -0400

    Fix Firebase v9+ import for getFirestore

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,4 +1,6 @@
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";  // Assurez-vous que cette ligne est présente
+import { getAuth } from "firebase/auth";  // Si vous avez besoin de l'authentification
 
 // Configuration Firebase
 const firebaseConfig = {
@@ -8,11 +10,14 @@ const firebaseConfig = {
   storageBucket: "zoom-actu.firebasestorage.app",
   messagingSenderId: "174263526950",
   appId: "1:174263526950:web:e5c52d59b3e9c16e26e8e7",
+  measurementId: "G-75JK7R6F96"
 };
 
 // Initialisation de Firebase
 const app = initializeApp(firebaseConfig);
-const firestore = getFirestore(app);
 
-// Exportation de la connexion à Firestore
-export { firestore };
+// Initialisation de Firestore et Auth
+const firestore = getFirestore(app);
+const auth = getAuth(app);
+
+export { firestore, auth };
